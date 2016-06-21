8c24c865201b907e7073a3e12fb3772429b15015
LeoMain <lvsdom@mail.ru>
2016-06-21 17:43:53 +0500

    Added model & base rest

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 var app = require('koa')();
 var router = require('koa-router')();
+var bodyParser = require('koa-bodyparser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://192.168.99.100:27017/test');
+app.use(bodyParser());
 
-router.get('/', function *(next) {
-	this.body = 'get';
+var postSchema = mongoose.Schema({
+	author: Number,
+    name: String,
+    text: String,
+    allow: Boolean
 });
 
-router.post('/', function *(next) {
-	this.body = 'post';
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+var Post = mongoose.model('Post', postSchema);
+
+router.get('/posts', function *(next) {
+	var posts = yield Post.find().exec();
+    this.body = posts;
 });
 
-router.put('/', function *(next) {
-	this.body = 'put';
+router.get('/posts/:id', function *(next) {
+	var post = yield Post.findOne(this.params.id).exec();
+    this.body = post;
 });
 
-router.delete('/', function *(next) {
-	this.body = 'delete';
+router.post('/posts', function *(next) {
+	var post = new Post(this.request.body);
+	post.save(function (err) {
+		if (err) 
+			return console.error(err);
+	});
+});
+
+router.put('/posts/:id', function *(next) {
+	var query = {'_id': this.params.id};
+	Post.findOneAndUpdate(query, this.request.body, function(err, doc){
+    if (err) console.log("err");
+    console.log("succesfully saved");
+});
+});
+
+router.delete('/posts/:id', function *(next) {
+	Post.findOne({ _id: this.params.id }).remove().exec();
 });
 
 app.use(router.routes());
