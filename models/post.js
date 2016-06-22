76f7b62a2fcace7975eb9fa0472a87f3c86baceb
LeoMain <lvsdom@mail.ru>
2016-06-22 15:51:04 +0500

    Added model validation

--- models/post.js
+++ models/post.js
@@ -1,9 +1,22 @@
 var mongoose = require('mongoose');
 
 var postSchema = mongoose.Schema({
-	author: Number,
-	name: String,
-	text: String,
+	author: {
+		type: Number,
+		min: 1,
+		required: true
+	},
+	name: {
+		type: String,
+		minlength: 10,
+		required: true
+	},
+	text: {
+		type: String,
+		minlength: 10,
+		maxlength: 1000,
+		required: true
+	},
 	allow: Boolean
 });
 
