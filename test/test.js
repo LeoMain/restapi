7a5d23dba2a18263771e9f139cd74d84de30cab4
LeoMain <lvsdom@mail.ru>
2016-06-23 14:00:45 +0500

    Turn test from callbacks to generators

--- test/test.js
+++ test/test.js
@@ -3,23 +3,23 @@ var chaiHttp = require('chai-http');
 var server = require('../index').listen(3000);
 var should = chai.should();
 var Post = require('../models/post');
-
+var mocha = require('mocha');
+var coMocha = require('co-mocha');
 chai.use(chaiHttp);
 
 
-describe('Blobs', function() {
-	it('get all posts on /posts GET', function(done) {
-		chai.request(server)
-			.get('/posts')
-			.end(function(err, res){
-				res.should.have.status(200);
-				res.should.be.json;
-				res.body.should.be.a('array');
-				done();
-			});
+describe('Posts', function() {
+	it('get all posts on /posts GET', function* (done) {
+		var res = yield chai.request(server)
+			.get('/posts');
+
+		res.should.have.status(200);
+		res.should.be.json;
+		res.body.should.be.a('array');
+		done();
 	});
 
-	it('get post on /posts/:id GET', function(done) {
+	it('get post on /posts/:id GET', function* (done) {
 		var post = new Post({
 			"author": 1,
 			"name": "Jack Nikolson",
@@ -27,19 +27,17 @@ describe('Blobs', function() {
 			"allow": false
 		});
 
-		post.save(function(err, data) {
-			chai.request(server)
-				.get('/posts/'+data.id)
-				.end(function(err, res) {
-					res.should.have.status(200);
-					res.should.be.json;
-					res.body.should.be.a('object');
-					done();
-				});
-		});
+		yield post.save();
+		var res = yield chai.request(server)
+			.get('/posts/'+post.id);
+
+		res.should.have.status(200);
+		res.should.be.json;
+		res.body.should.be.a('object');
+		done();
 	});
 
-	it('add post on /posts POST', function(done) {
+	it('add post on /posts POST', function* (done) {
 		var post = {
 			"author": 1,
 			"name": "Jack Nikolson",
@@ -47,18 +45,17 @@ describe('Blobs', function() {
 			"allow": false
 		};
 
-		chai.request(server)
+		var res = yield chai.request(server)
 			.post('/posts')
-			.send(post)
-			.end(function(err, res) {
-				res.should.have.status(200);
-				res.should.be.json;
-				res.body.should.be.a('object');
-				done();
-			});
+			.send(post);
+
+		res.should.have.status(200);
+		res.should.be.json;
+		res.body.should.be.a('object');
+		done();
 	});
 
-	it('edit post on /posts/:id PUT', function(done) {
+	it('edit post on /posts/:id PUT', function* (done) {
 		var post = {
 			"author": 1,
 			"name": "Anton Petrov",
@@ -66,38 +63,34 @@ describe('Blobs', function() {
 			"allow": false
 		};
 
-		chai.request(server)
-			.get('/posts')
-			.end(function(err, res){
-				chai.request(server)
-					.put('/posts/'+res.body[res.body.length-1]._id)
-					.send(post)
-					.end(function(err, res) {
-						res.should.have.status(200);
-						res.should.be.json;
-						res.body.should.be.a('object');
-						res.body.author.should.equal(1);
-						res.body.name.should.equal("Anton Petrov");
-						res.body.text.should.equal("Some test message");
-						res.body.allow.should.equal(false);
-						done();
-					});
-			});
+		var posts = yield chai.request(server)
+			.get('/posts');
+
+		var res = yield chai.request(server)
+			.put('/posts/'+posts.body[posts.body.length-1]._id)
+			.send(post);
+
+		res.should.have.status(200);
+		res.should.be.json;
+		res.body.should.be.a('object');
+		res.body.author.should.equal(1);
+		res.body.name.should.equal("Anton Petrov");
+		res.body.text.should.equal("Some test message");
+		res.body.allow.should.equal(false);
+		done();
 	});
 
-	it('delete post on /posts/:id DELETE', function(done) {
-		chai.request(server)
-			.get('/posts')
-			.end(function(err, res){
-				chai.request(server)
-					.delete('/posts/'+res.body[res.body.length-1]._id)
-					.end(function(err, res) {
-						res.should.have.status(200);
-						res.should.be.json;
-						res.body.should.be.a('object');
-						res.body.n.should.equal(1);
-						done();
-					});
-			});
+	it('delete post on /posts/:id DELETE', function* (done) {
+		var posts = yield chai.request(server)
+			.get('/posts');
+
+		var res = yield chai.request(server)
+			.delete('/posts/'+posts.body[posts.body.length-1]._id);
+
+		res.should.have.status(200);
+		res.should.be.json;
+		res.body.should.be.a('object');
+		res.body.n.should.equal(1);
+		done();
 	});
 });
